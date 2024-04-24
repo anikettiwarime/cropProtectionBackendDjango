d9624e88a0bd0090d2932add423eebd716ae02f5
Aniket Tiwari <anikettiwari1570@gmail.com>
2024-04-24 13:45:39 +0530

    Update ALLOWED_HOSTS in core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [
-                           s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['cropprotectionbackenddjango.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
